061e7a184d8421a0c8632d6f5c6ff4892216782d
MarcoBusto <msbusto@gmail.com>
2022-12-15 19:40:55 -0300

    15/12/2022

--- main.js
+++ main.js
@@ -1,41 +1,29 @@
-const anual30=30;
-const anual40=40;
-const anual50=50;
-let resultado=0;
-let monto=parseFloat(prompt("¡Bienvenido! Si desea salir, ingrese la palabra salir Ingrese el monto del credito a solicitar"));
+const anual30 = 30;
+const anual40 = 40;
+const anual50 = 50;
+const impuesto=5;
+let resultado = 0;
 
+
+
+let monto = parseFloat(prompt("Ingrese el monto del credito a solicitar, para finalizar escriba '0':"));
 /*Funciones*/
-
-function interes30(){
-    resultado=monto+parseFloat((monto*anual30)/100);
-    alert("El monto a devolver en un año es de:"+resultado);    
+function calcularInteres(interes) {
+        resImp= (monto * impuesto)/100;
+        resultado = (monto * (interes/100))+resImp+monto;
+                alert("El monto a devolver en un año es de:" + resultado);
 }
 
-function interes40(){
-    resultado=monto+parseFloat((monto*anual40)/100);
-    alert("El monto a devolver en un año es de:"+resultado);    
-}
-
-function interes50(){
-   resultado=monto+parseFloat((monto*anual50)/100);
-   alert("El monto a devolver en un año es de:"+""+resultado);    
-}
-
-	     
- while (monto !== "salir"){
-        if(monto<=50000){
-            interes50();
-            alert("¡Gracias por confiar en nosotros!");
-        } else if(monto > 50000 && monto <= 100000){
-            interes40();
-            alert("¡Gracias por confiar en nosotros!");
-        } else if(monto > 100000) {
-            interes50();
-            alert("¡Gracias por confiar en nosotros!");
-        } 
-    monto=parseFloat(prompt("Ingrese el monto del credito a solicitar"));
+while (monto != 0) {
+    if (monto <= 50000) {
+            calcularInteres(anual50);
+    }   else if (monto > 50000 && monto <= 100000) {
+            calcularInteres(anual40);
+    }   else if (monto > 100000) {
+            calcularInteres(anual30);
     }
-    
-    
+    alert("¡Gracias por confiar en nosotros!");
+    monto = parseFloat(prompt("Ingrese el monto del credito a solicitar, para finalizar escriba '0':"));
+}
     
 
