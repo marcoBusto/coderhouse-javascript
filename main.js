2a4520de1889686bc565969105ae475902f38e38
MarcoBusto <msbusto@gmail.com>
2022-12-21 15:18:29 -0300

    preentrega2

--- main.js
+++ main.js
@@ -1,34 +1,57 @@
+//Constantes
 const anual30 = 30;
 const anual40 = 40;
 const anual50 = 50;
 const impuesto=5;
-let resultado = 0;
-let listadoRecargos=[]
 
-
-let monto = parseFloat(prompt("Ingrese el monto del credito a solicitar, para finalizar escriba '0':"));
-/*Funciones*/
-function calcularInteres(interes) {
-        resImp= (monto * impuesto)/100;
-        resultado = (monto * (interes/100))+resImp+monto;
-                alert("El monto a devolver en un año es de:" + resultado);
-}
-function mostrarListado{ 
-        var lista='';
-        for(let i=0; i<listadoRecargos.length; i++){
-        
+//clases
+ class Prestamo {
+        constructor(monto){
+               this.monto=monto;
+               this.montofinal = 0;
         }
-    }
-while (monto != 0) {
-    if (monto <= 50000) {
-            calcularInteres(anual50);
-    }   else if (monto > 50000 && monto <= 100000) {
-            calcularInteres(anual40);
-    }   else if (monto > 100000) {
-            calcularInteres(anual30);
-    }
-    alert("¡Gracias por confiar en nosotros!");
-    monto = parseFloat(prompt("Ingrese el monto del credito a solicitar, para finalizar escriba '0':"));
+        calcularInteres(interes) {
+                let resImp= (this.monto * impuesto)/100;
+                this.montofinal = (this.monto * (interes/100))+resImp+this.monto;
+                console.log("El monto a devolver en un año es de:" + this.montofinal);
+        }
+        solicitarMonto(){
+                this.monto = parseFloat(prompt("Ingrese el monto del credito a solicitar, para finalizar escriba '0':"));
+                console.log(this.monto);
+        }
+        calcularPrestamo(){
+                if (this.monto <= 50000) {
+                        this.calcularInteres(anual50);
+                }else if (this.monto > 50000 && this.monto <= 100000) {
+                        this.calcularInteres(anual40);
+                }else if (this.monto > 100000) {
+                        this.calcularInteres(anual30);
+                }
+        }
 }
-    
+class Cliente{
+        constructor (nombre,apellido,direccion,telefono,email){
+        this.nombre= nombre;
+        this.apellido= apellido;
+        this.direccion= direccion;
+        this.telefono= telefono;
+        this.email= email;
+        }
+}
+const datosCliente = new Cliente("Marco","Busto","Belgrano 613","34724-30648","msbusto@gmail.com");
 
+
+const cantidad = parseFloat(prompt("Ingrese la cantidad de préstamos que quiere obtener':"));
+const prestamos=[];
+for (let i=1; i<= cantidad; i++){
+        let auxPrestamo    = new Prestamo(0);
+        auxPrestamo.calcularInteres();
+        auxPrestamo.solicitarMonto();
+        auxPrestamo.calcularPrestamo();
+        prestamos.push(auxPrestamo);
+        alert("El monto final a pagar es: " + auxPrestamo.montofinal);
+}
+
+alert("¡Gracias por confiar en nosotros!");
+console.log(datosCliente);
+console.table(prestamos);
